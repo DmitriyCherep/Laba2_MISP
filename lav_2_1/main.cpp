1f70859321b84281f9fcd285bf464d4ca88e4af6
Dmitriy Borisov <113330319+DmitriyCherep@users.noreply.github.com>
2023-01-24 20:47:38 +0300

    Update main.cpp

--- lav_2_1/main.cpp
+++ lav_2_1/main.cpp
@@ -1,41 +1,39 @@
 #include <iostream>
 #include <cctype>
-#include "Cipher.h"
+#include "src/lib/modAlphaCipher.hpp"
+#include "Windows.h"
 using namespace std;
-void check(const wstring& Text, const wstring& key, const bool destructCipherText=false)
-{
-    try {
-        wstring cipherText;
-        wstring decryptedText;
-        Cipher cipher(key);
-        cipherText = cipher.encrypt(Text);
-        if (destructCipherText)
-            cipherText.front() = tolower(cipherText.front());
-        decryptedText = cipher.decrypt(cipherText);
-        wcout<<"key="<<key<<endl;
-        wcout<<Text<<endl;
-        wcout<<cipherText<<endl;
-        wcout<<decryptedText<<endl;
-    } catch (const cipher_error & e) {
-        cerr<<"Error: "<<e.what()<<endl;
-    }
-}
 int main(int argc, char **argv)
 {
-    locale loc("ru_RU.UTF-8");
-    locale::global(loc);
-    wcout<<L'-'<<endl;
-    check(L"ЭТОРАНДОМНЫЙТЕКСТ",L"ПРИВЕТ");
-    wcout<<L'-'<<endl;
-    check(L"ЭТОРАНДОМНЫЙТЕКСТ",L"");
-    wcout<<L'-'<<endl;
-    check(L"ЭТОРАНДОМНЫЙТЕКСТ",L"П1Р2И3В4Е5Т6");
-    wcout<<L'-'<<endl;
-    check(L"ЭТО РАНДОМНЫЙ ТЕКСТ",L"ПРИВЕТ");
-    wcout<<L'-'<<endl;
-    check(L"ЭТОРАНДОМНЫЙТЕКСТ",L"ПРИВЕТ",true);
-    wcout<<L'-'<<endl;
-    check(L"21908321786912367801327860",L"ПРИВЕТ");
-    wcout<<L'-'<<endl;
-    return 0;
+ SetConsoleCP(1251);
+ SetConsoleOutputCP(1251);
+ std::string dictionary = modAlphaCipher::generateDict();
+ string key;
+ string text;
+ unsigned op;
+ cout<<"Cipher ready. Input key: ";
+ cin>>key;
+ try {
+ modAlphaCipher cipher(key, dictionary);
+ do {
+ cout<<"Cipher ready. Input operation (0-exit, 1-encrypt, 2-decrypt): ";
+ cin>>op;
+ if (op > 2) {
+ cout<<"Illegal operation\n";
+ } else if (op > 0) {
+ cout<<"Cipher ready. Input text: ";
+ cin>>text;
+ if (op == 1) {
+ cout<<"Encrypted text:
+"<<cipher.encrypt(text)<<endl;
+ } else {
+ cout<<"Decrypted text: "<<cipher.decrypt(text)<<endl;
+ }
+ }
+ } while (op!=0);
+ }
+ catch(const cipher_error& e) {
+ cerr << "Error: " << e.what() << endl;
+ }
+ return 0;
 }
