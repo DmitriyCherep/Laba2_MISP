7a28f629f8678756bbd184a5ce6d69ba01bf8cab
Dmitriy Borisov <113330319+DmitriyCherep@users.noreply.github.com>
2023-01-24 20:48:06 +0300

    Update Cipher.h

--- lav_2_2/Cipher.h
+++ lav_2_2/Cipher.h
@@ -1,26 +1,25 @@
 #pragma once
-#include <string>
 #include <vector>
-#include <iostream>
-using namespace std;
-class Cipher
+#include <string>
+#include <stdexcept>
+class routeCipher
 {
-private:
-    int k;
-    wstring getValidOpenText(const std::wstring & s);
-    wstring getValidCipherText(const std::wstring & s);
-    int getValidKey(const int k, const std::wstring & s);
-public:
-    Cipher()=delete;
-    Cipher(int w);
-    wstring zakodirovat(Cipher w, wstring& s);
-    wstring raskodirovar(Cipher w, wstring& s);
+ private:
+ int columns;
+ int div_up(int x, int y);
+ std::string getValidKey(const std::string& s);
+ std::string getValidOpenText(const std::string& s);
+ std::string getValidCipherText(const std::string& s);
+ public:
+ routeCipher()=delete;
+ routeCipher(const std::string& key);
+ std::string encrypt(const std::string& open_text);
+ std::string decrypt(const std::string& cipher_text);
 };
-class cipher_error: public std::invalid_argument
-{
-public:
-    explicit cipher_error (const std::string& what_arg):
-        std::invalid_argument(what_arg) {}
-    explicit cipher_error (const char* what_arg):
-        std::invalid_argument(what_arg) {}
+class cipher_error: public std::invalid_argument {
+ public:
+ explicit cipher_error (const std::string& what_arg):
+ std::invalid_argument(what_arg) {}
+ explicit cipher_error (const char* what_arg):
+ std::invalid_argument(what_arg) {}
 };
